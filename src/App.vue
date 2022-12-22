a93a0bef7e86ca04cb54d6b7168c35f025129dd0
gryhkn <girayhakan23@gmail.com>
2022-12-22 14:16:39 +0300

    Project created.

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="bg-black w-full h-screen">
+    <h1>hello</h1>
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="bg-black">
+    <h1>Helo</h1>
+  </div>
+</template>
