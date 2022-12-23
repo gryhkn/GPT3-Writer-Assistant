5bd276701646cf6f4b9dee9416a0b5bee1929ed7
gryhkn <girayhakan23@gmail.com>
2022-12-23 20:39:41 +0300

    Front-end finished.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
-<script setup></script>
-
 <template>
   <div class="bg-black w-full h-screen">
-    <h1>hello</h1>
+    <div class="flex items-center justify-center flex-col w-auto">
+      <div class="flex flex-col relative">
+        <div>
+          <h1 class="text-white text-8xl font-bold pt-20">
+            GPT-3 Writing Assistant
+          </h1>
+        </div>
+        <div class="flex items-center justify-center">
+          <h2 class="pt-6 not-italic font-normal text-2xl text-gray-400">
+            insert your subtitle here kjnsdf ksdkfjn skjnfksjn ksjndfs
+          </h2>
+        </div>
+      </div>
+      <div class="pt-10 relative">
+        <div>
+          <div>
+            <textarea
+              name="promp-input"
+              id=""
+              cols="60"
+              rows="10"
+              class="rounded-2xl m-2 p-4 bg-gray-900 h-36 text-white border-blue-300 border"
+              placeholder="Yes, Sir!"
+            ></textarea>
+          </div>
+        </div>
+        <div class="mt-10">
+          <button
+            class="bg-white absolute bottom-0 right-0 p-2 rounded-2xl hover:shadow-green-600 hover:shadow-xl shadow-green-500 shadow-lg"
+            @click="handleClick"
+          >
+            Generate
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+const handleClick = () => {
+  console.log("Hello");
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,23 @@
 <script setup></script>
 
 <template>
-  <div class="bg-black">
-    <h1>Helo</h1>
+  <div class="bg-black h-screen">
+    <div class="container mx-auto px-4 py-10">
+      <h1 class="text-center text-6xl font-bold text-white mt-10">
+        GPT-3 Writing Assistant
+      </h1>
+      <p class="text-center text-lg text-white mt-4">Write your command here</p>
+      <div class="relative mx-auto w-4/5">
+        <textarea
+          class="h-32 px-4 py-4 rounded-lg shadow-lg bg-white text-gray-800 font-medium"
+          rows="10"
+        ></textarea>
+        <button
+          class="absolute bottom-0 right-0 bg-orange-500 px-4 py-2 rounded-full mt-4 text-white font-medium shadow-lg hover:bg-orange-600"
+        >
+          Generate
+        </button>
+      </div>
+    </div>
   </div>
 </template>
